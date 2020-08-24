0de4c3637669ea208b1e194f3170234594f8fb4c
Michael Meshcheryakov <m.meshcheryakov@social-solutions.ru>
2020-08-24 17:35:47 +0500

    Added project progress line Added project overview part

---
+++ src/components/project/ProjectOverview.vue
@@ -0,0 +1,122 @@
+<template>
+   <div class="project-overview">
+      <div class="overview-container">
+         <div class="overview-header">
+            PLEASE CHECK THE CATEGORIES
+            YOU WANT TO SHOW
+         </div>
+         <div class="overview-checkboxes">
+            <div class="overview-checkbox" :class="{active: showUnits}">
+               <label for="show-units"><div class="overview-checkbox-mask"><div class="overview-checkbox-icon"></div></div> Floor Plates and Units</label>
+               <input type="checkbox" v-on:change="changeUnits()" v-model="showUnits" id="show-units" style="display: none">
+            </div>
+            <div class="overview-checkbox" :class="{active: showMap}">
+               <label for="show-map"><div class="overview-checkbox-mask"><div class="overview-checkbox-icon"></div></div>Neighborhood Maps</label>
+               <input type="checkbox" v-on:change="changeMap()" v-model="showMap" id="show-map" style="display: none">
+            </div>
+            <div class="overview-checkbox" :class="{active: showGallery}">
+            <label for="show-gallery"><div class="overview-checkbox-mask"><div class="overview-checkbox-icon"></div></div>Galleries</label>
+            <input type="checkbox" v-on:change="changeGallery()" v-model="showGallery" id="show-gallery" style="display: none">
+         </div>
+         </div>
+      </div>
+      <div class="overview-container">
+         <div class="overview-header">
+            PROJECT TITLE *
+         </div>
+         <div class="overview-description">This is the name that the user will see in the app.</div>
+         <input class="overview-input" :class="{warning: projectName.length < 1}" type="text" v-model="projectName" @keyup="enterName()">
+         <div class="overview-logo">
+            <div class="overview-header">
+               PROJECT LOGO
+            </div>
+            <div class="overview-description">The graphics will appear on the splash screen</div>
+            <div class="logo-preview"></div>
+            <div class="logo-upload" v-on:click="openPhotoUpload()">Click to upload</div>
+            <input type="file" v-on:change="renderPreview" id="overview-logo" style="display: none" accept="image/png, image/jpeg">
+         </div>
+      </div>
+   </div>
+</template>
+
+<script>
+  export default {
+    'name': 'ProjectOverview',
+    data: () => ({
+      projectName: '',
+      projectLogo: '',
+      showUnits: true,
+      showMap: true,
+      showGallery: true,
+    }),
+    mounted() {
+      this.projectName = this.$parent.project.name;
+      this.projectLogo = this.$parent.project.logo;
+      if(this.projectName.length > 0) {
+        this.$parent.progresses[5].active = true;
+      } else {
+        this.$parent.progresses[5].active = false;
+      }
+      this.changeUnits();
+      this.changeMap();
+      this.changeGallery();
+    },
+    methods: {
+      openPhotoUpload() {
+        var container = document.getElementById('overview-logo');
+        container.click();
+      },
+      renderPreview: function (e) {
+        var file = e.target.files[0];
+      },
+      enterName() {
+        this.$parent.project.name = this.projectName;
+        if(this.projectName.length > 0) {
+          this.$parent.progresses[5].active = true;
+        } else {
+          this.$parent.progresses[5].active = false;
+        }
+        if(this.showUnits === true && this.projectName.length > 0) {
+          this.$parent.progresses[1].active = true;
+          this.$parent.progresses[2].active = true;
+        } else {
+          this.$parent.progresses[1].active = false;
+          this.$parent.progresses[2].active = false;
+        }
+        if(this.showMap === true && this.projectName.length > 0) {
+          this.$parent.progresses[3].active = true;
+        } else {
+          this.$parent.progresses[3].active = false;
+        }
+        if(this.showGallery === true && this.projectName.length > 0) {
+          this.$parent.progresses[4].active = true;
+        } else {
+          this.$parent.progresses[4].active = false;
+        }
+      },
+      changeUnits() {
+        if(this.showUnits === true && this.projectName.length > 0) {
+          this.$parent.progresses[1].active = true;
+          this.$parent.progresses[2].active = true;
+        } else {
+          this.$parent.progresses[1].active = false;
+          this.$parent.progresses[2].active = false;
+        }
+      },
+      changeMap() {
+        if(this.showMap === true && this.projectName.length > 0) {
+          this.$parent.progresses[3].active = true;
+        } else {
+          this.$parent.progresses[3].active = false;
+        }
+      },
+      changeGallery() {
+        if(this.showGallery === true && this.projectName.length > 0) {
+          this.$parent.progresses[4].active = true;
+        } else {
+          this.$parent.progresses[4].active = false;
+        }
+      },
+    },
+  }
+</script>
--- src/views/NewProject.vue
+++ src/views/NewProject.vue
@@ -1,11 +1,95 @@
 <template>
-    <div class="">
-        New Project is here
+    <div class="new-project-page">
+        <div class="new-project-progress">
+            <div class="progress-tab" v-for="(progress, key) in progresses"
+                 :class="{inactive: progress.active != true}">
+                <div class="progress-name" v-on:click="makeActive(key + 1)">{{key + 1}} {{progress.name}}</div>
+                <div class="progress-arrow" v-if="progress.arrow"></div>
+            </div>
+        </div>
+        <div class="project-page-content">
+            <ProjectOverview v-bind:name="project.name" v-show="activeTab == 1"></ProjectOverview>
+        </div>
+        <div class="project-page-controls" v-if="!published">
+            <div class="project-page-button" :class="{invisible: activeTab == 1}">Back</div>
+            <div class="project-page-subcontrols" v-if="project.name.length > 0">
+                <div class="project-page-button">Save Draft</div>
+                <div class="project-page-button blue-button" v-if="activeTab != 6">Next</div>
+                <div class="project-page-button blue-button" v-if="activeTab == 6">Publish</div>
+            </div>
+        </div>
+        <div class="project-page-publish" v-if="published">
+            <div class="publish-button">
+                <span class="publish-icon"></span>
+                Published
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+  import ProjectOverview from '@/components/project/ProjectOverview.vue'
   export default {
     name: 'newproject',
+    components: {
+      ProjectOverview,
+    },
+    mounted() {
+      var tabs = document.getElementsByClassName('progress-tab');
+      tabs[this.activeTab - 1].classList.add('active');
+    },
+    methods: {
+      makeActive(e) {
+        if (this.progresses[e - 1].active === true) {
+          this.activeTab = e;
+          var tabs = document.getElementsByClassName('progress-tab');
+          for (var i = 0; i < tabs.length; i++) {
+            tabs[i].classList.remove('active');
+          }
+          tabs[this.activeTab - 1].classList.add('active');
+        }
+      }
+    },
+    data: ()=>({
+      activeTab: 1,
+      tabsCount: 1,
+      published: false,
+      progresses: [
+        {
+          name: 'Overview',
+          arrow: true,
+          active: true,
+        },
+        {
+          name: 'Floor Plates',
+          arrow: true,
+          active: false,
+        },
+        {
+          name: 'Units',
+          arrow: true,
+          active: false,
+        },
+        {
+          name: 'Maps',
+          arrow: true,
+          active: false,
+        },
+        {
+          name: 'Galleries',
+          arrow: true,
+          active: false,
+        },
+        {
+          name: 'Publish',
+          arrow: false,
+          active: false,
+        },
+      ],
+      project: {
+        name: 'Васян',
+        logo: '',
+      },
+    }),
   }
 </script>
