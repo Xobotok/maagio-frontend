8283fc8af8ffe487be8c6c72075dd7736b8ce6e5
Michael Meshcheryakov <m.meshcheryakov@social-solutions.ru>
2020-08-24 10:51:00 +0500

    Added project card. Added empty image Added 1 project in hardcode

---
+++ src/components/project/ProjectCard.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="project-card">
+        <div class="project-content">
+            <div class="image-container">
+                <div class="project-image"  :style="{ backgroundImage: 'url('+getImgUrl()+')' }"></div>
+            </div>
+            <div class="project-name">
+                {{project.name}}
+            </div>
+            <div class="project-menu">
+                <div class="project-menu-text">EDIT</div>
+                <div class="menu-delimiter"></div>
+                <div class="project-menu-text">
+                    PREVIEW
+                    <span class="menu-link"></span>
+                </div>
+                <div class="menu-delimiter"></div>
+                <div class="project-menu-icons">
+                    <div class="menu-share"></div>
+                    <div class="menu-delete"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+  export default {
+    'name': 'ProjectCard',
+    props: ['project'],
+    methods: {
+      getImgUrl() {
+        return require('../../assets/'+this._props.project.img);
+      }
+    },
+    mounted: function () {
+    }
+  }
+</script>
--- src/views/MyProjects.vue
+++ src/views/MyProjects.vue
@@ -1,18 +1,28 @@
 <template>
-    <div class="projects">
-        <ProjectEmptyCard />
-        <ProjectEmptyCard />
-        <ProjectEmptyCard />
-        <ProjectEmptyCard />
+    <div class="projects" id="projects">
+        <ProjectEmptyCard v-if="projects.length == 0"></ProjectEmptyCard>
+        <ProjectCard v-for="project in projects" v-bind:project="project"></ProjectCard>
     </div>
 </template>
 
 <script>
     import ProjectEmptyCard from '@/components/project/ProjectEmptyCard.vue'
+    import ProjectCard from '@/components/project/ProjectCard.vue'
   export default {
     name: 'myprojects',
     components: {
-      ProjectEmptyCard,
-    }
+      ProjectEmptyCard,ProjectCard
+    },
+    data: ()=>({
+        projects: [
+          {
+            id: 1,
+            name: 'Васян',
+            img: 'img/no-image-project.png',
+          },
+          ],
+    }),
+    methods: {
+    },
   }
 </script>
