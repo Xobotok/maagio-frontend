0f49ee2588dcddfaf80dfc182331c0fea910f92e
Michael Meshcheryakov <m.meshcheryakov@social-solutions.ru>
2020-08-28 18:52:21 +0500

    Finished gallery frontend and gallery modal frontend

--- src/components/project/NewGalleryModal.vue
+++ src/components/project/NewGalleryModal.vue
@@ -7,29 +7,28 @@
                     <div class="gallery-header">
                         <span>Name*</span>
                     </div>
-                    <div class=""><input type="text" class="project-input">
+                    <div class=""><input type="text" class="project-input" v-model="gallery.name">
                     </div>
                 </div>
                 <div class="photos-container">
                     <div class="gallery-header"><span>Photos</span> Please add at least one photo.</div>
                     <div class="gallery-photos">
-                        <div class="gallery-photo" v-for="photo in photos">
-                            <img src="../../assets/img/no-image-project.png" alt="">
+                        <div class="gallery-photo" v-for="photo in gallery.photos">
+                            <img :src="photo.link" alt="">
                             <div class="photo-controls">
                                 <div class="photo-controls-button">REPLACE</div>
                                 <div class="photo-controls-button" @click="removePhoto(photo)">REMOVE</div>
                             </div>
                         </div>
-                        <div class="gallery-photo">
-                            <img src="../../assets/img/no-image-project.png" alt="">
-                        </div>
-                        <div class="gallery-photo">
-                            <img src="../../assets/img/no-image-project.png" alt="">
-                        </div>
                         <div class="empty-floor" onclick="document.getElementById('photo-upload').click()"><div class="empty-floor-text"><span class="empty-floor-plus">+</span> Add</div></div>
                         <input type="file" style="display: none" multiple accept="image/png, image/jpeg" id="photo-upload">
                     </div>
                 </div>
+            </div>
+            <div class="unit-create-controls">
+                <div class="project-page-button blue-button" @click="saveGallery" v-if="checkSave()">Save</div>
+                <div class="project-page-button inactive-button" v-if="!checkSave()">Save</div>
+                <div class="project-page-button" @click="closeModal">Cancel</div>
             </div>
         </div>
     </div>
@@ -39,30 +38,62 @@
   export default {
     name: 'NewGalleryModal',
     data: ()=>({
-      photos: [
-        {
-          link: '',
-          number: 1,
-        },
-        {
-          link: '',
-          number: 2,
-        },
-        {
-          link: '',
-          number: 3,
-        }
-      ],
+      gallery: {
+        photos: [
+          {
+            link: require('../../assets/img/no-image-project.png'),
+            number: 1,
+          },
+          {
+            link: require('../../assets/img/no-image-project.png'),
+            number: 2,
+          },
+          {
+            link: require('../../assets/img/no-image-project.png'),
+            number: 3,
+          }
+        ],
+        name: '',
+      },
     }),
     methods: {
+      checkSave() {
+        if(this.gallery.photos.length > 0 && this.gallery.name !== '') {
+          return true;
+        } else {
+          return false;
+        }
+      },
+      closeModal() {
+        this.$parent.openModal = false;
+        console.log(this.$parent.galleries);
+      },
+      saveGallery() {
+        if(this.checkSave()) {
+          let flag = false;
+          for(var i = 0; i < this.$parent.galleries.length; i++) {
+            if(this.$parent.galleries[i] === this.gallery) {
+              this.$parent.galleries[i] = this.gallery;
+              flag = true;
+            }
+          }
+          if(!flag) {
+            this.$parent.galleries.push({name: this.gallery.name, photos: this.gallery.photos});
+          }
+
+        }
+        this.closeModal();
+      },
       removePhoto(el) {
-        for(let i = 0; i < this.photos.length; i++) {
-          if(el === this.photos[i]) {
-            this.photos.splice(i, 1);
+        for(let i = 0; i < this.gallery.photos.length; i++) {
+          if(el === this.gallery.photos[i]) {
+            this.gallery.photos.splice(i, 1);
           }
         }
-        console.log(el);
       }
+    },
+    mounted() {
+        this.gallery = this.$parent.tempGallery;
     },
   }
 </script>
--- src/components/project/ProjectGalleries.vue
+++ src/components/project/ProjectGalleries.vue
@@ -1,7 +1,19 @@
 <template>
     <div class="project-galleries">
         <div class="galleries-container">
-            <div class="empty-floor" @click="openModal = true"><div class="empty-floor-text"><span class="empty-floor-plus">+</span> Add</div></div>
+            <div class="gallery-item" v-for="gallery in galleries">
+                <div class="photo-container">
+                    <img :src="gallery.photos[0].link" alt="">
+                </div>
+                <div class="gallery-panel">
+                    <div class="gallery-name">{{gallery.name}}</div>
+                    <div class="gallery-controls">
+                        <div class="gallery-edit" @click="editGallery(gallery)"></div>
+                        <div class="gallery-remove" @click="removeGallery(gallery)"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="empty-floor" @click="createNewGallery"><div class="empty-floor-text"><span class="empty-floor-plus">+</span> Add</div></div>
         </div>
         <NewGalleryModal v-if="openModal === true"></NewGalleryModal>
     </div>
@@ -17,6 +29,48 @@
     data:()=>({
       galleries: [],
       openModal: false,
+      tempGallery: {
+        photos: [
+          {
+            link: require('../../assets/img/no-image-project.png'),
+            number: 1,
+          },
+          {
+            link: require('../../assets/img/no-image-project.png'),
+            number: 2,
+          },
+          {
+            link: require('../../assets/img/no-image-project.png'),
+            number: 3,
+          }
+        ],
+        name: '',
+      },
     }),
+    methods: {
+      createNewGallery() {
+        this.tempGallery = {
+          photos: [
+            {
+              link: require('../../assets/img/no-image-project.png'),
+              number: 1,
+            },
+          ],
+          name: '',
+        };
+        this.openModal = true;
+      },
+      editGallery(gallery) {
+        this.tempGallery = gallery;
+        this.openModal = true;
+      },
+      removeGallery(gallery) {
+        for(let i = 0; i < this.galleries.length; i++) {
+          if(this.galleries[i] === gallery) {
+            this.galleries.splice(i, 1);
+          }
+        }
+      },
+    }
   }
 </script>
