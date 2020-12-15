88c2f46471218da6bbb6c8848f02be4f0a606174
Michael Meshcheryakov <m.meshcheryakov@social-solutions.ru>
2020-12-15 13:25:33 +0500

    Redirect to https. Added PWA with dynamic start url

--- src/service-worker.js
+++ src/service-worker.js
@@ -10,7 +10,6 @@
  * and re-run your build process.
  * See https://goo.gl/2aRDsh
  */
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 workbox.core.setCacheNameDetails({prefix: "maagio"});
 var CACHE_NAME = 'my-site-cache-v1';
 self.addEventListener('fetch', function(event) {
